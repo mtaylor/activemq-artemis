5caf734918d3389961650c64d2200b3e3475e7c8
Martyn Taylor <mtaylor@redhat.com>
2015-09-11 10:43:59 +0100

    JDBC Work

--- tests/integration-tests/src/test/java/org/apache/activemq/artemis/tests/integration/jdbc/store/ActiveMQJDBCStoreTest.java
+++ tests/integration-tests/src/test/java/org/apache/activemq/artemis/tests/integration/jdbc/store/ActiveMQJDBCStoreTest.java
@@ -1,9 +1,5 @@
 package org.apache.activemq.artemis.tests.integration.jdbc.store;
 
-import org.junit.Test;
-
-import static junit.framework.TestCase.fail;
-
 public class ActiveMQJDBCStoreTest
 {
 //   @Test
