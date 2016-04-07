f2fcc1b289f1d3f7aedd32cc6a372a41e66d610d
Martyn Taylor <mtaylor@redhat.com>
2016-04-07 21:20:00 +0100

    Add default close() on page to allow CLI to compile

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/paging/impl/Page.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/paging/impl/Page.java
@@ -216,6 +216,10 @@ public final class Page implements Comparable<Page> {
       file.position(0);
    }
 
+   public void close() throws Exception {
+      close(true);
+   }
+   
    /** sendEvent means it's a close happening from a major event such moveNext.
     *  While reading the cache we don't need (and shouldn't inform the backup */
    public synchronized void close(boolean sendEvent) throws Exception {
