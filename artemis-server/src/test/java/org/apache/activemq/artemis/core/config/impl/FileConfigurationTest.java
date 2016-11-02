bcbbc86856cbb9679ce6886852797b3360605730
Clebert Suconic <clebertsuconic@apache.org>
2016-11-01 21:38:02 -0400

    ARTEMIS-832 Openwire was ignoring data syncs.
    I'm also adding the possibility of sync on libaio, and not only relay on write-cache

--- artemis-server/src/test/java/org/apache/activemq/artemis/core/config/impl/FileConfigurationTest.java
+++ artemis-server/src/test/java/org/apache/activemq/artemis/core/config/impl/FileConfigurationTest.java
@@ -359,6 +359,8 @@ public class FileConfigurationTest extends ConfigurationImplTest {
       assertEquals(1234567, conf.getGlobalMaxSize());
       assertEquals(37, conf.getMaxDiskUsage());
       assertEquals(123, conf.getDiskScanPeriod());
+
+      assertEquals(false, conf.isJournalDatasync());
    }
 
    private void verifyAddresses() {
