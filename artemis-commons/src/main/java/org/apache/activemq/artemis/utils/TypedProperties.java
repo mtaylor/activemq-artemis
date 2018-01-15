292789c2a792eb3f2c53394f75958c3d87be861b
Martyn Taylor <mtaylor@redhat.com>
2018-01-15 16:36:01 +0000

    ARTEMIS-1611 Added support for 1.x transformer API

--- artemis-commons/src/main/java/org/apache/activemq/artemis/utils/TypedProperties.java
+++ artemis-commons/src/main/java/org/apache/activemq/artemis/utils/TypedProperties.java
@@ -23,4 +23,7 @@ package org.apache.activemq.artemis.utils;
 @Deprecated
 public class TypedProperties extends org.apache.activemq.artemis.utils.collections.TypedProperties {
 
+   public TypedProperties(final org.apache.activemq.artemis.utils.collections.TypedProperties other) {
+      super(other);
+   }
 }
